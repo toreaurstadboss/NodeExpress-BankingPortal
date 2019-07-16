66599cc5c83784e216f2ac9a2eefd16d2e1d5a0f
Tore Aurstad <tore.aurstad@gmail.com>
2019-07-16 17:22:23 +0200

    Completed the Module

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
+const writeJSON = () => {
+    var accountsJSON = JSON.stringify(accounts);
+     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json',), accountsJSON, 'utf-8');
+};
+
+module.exports = { accounts, users, writeJSON };
