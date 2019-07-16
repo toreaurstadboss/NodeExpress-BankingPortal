d0c0326ab778cf6b0de7d1b04ac84228de5bb64d
Tore Aurstad <tore.aurstad@gmail.com>
2019-07-16 15:50:55 +0200

    Completed the module

--- src/app.js
+++ src/app.js
@@ -4,10 +4,9 @@ const express = require('express');
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(request, response){
 
- response.render('index.ejs', { title: 'Index'});
+ response.render('index', { title: 'Index'});
 }); 
-app.listen(3000);
-console.log("PS Project Running on port 3000!");
+app.listen(3000, () => console.log("PS Project Running on port 3000!"));
